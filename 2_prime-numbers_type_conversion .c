968ade55d8e869576b3a61b3295f9cda916745e6
Vio-lettaB <126187797+Vio-lettaB@users.noreply.github.com>
2023-03-05 23:06:21 +0700

    Update and rename 2nd task privedenie 2.0 to 2_prime-numbers_type_conversion .c

---
+++ 2_prime-numbers_type_conversion .c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int n;
+int i;
+
+int prostota(int i)
+{
+    
+	for (int k = 2; k < i; k++)
+	{
+
+		float j;
+		float m;
+		float r;
+		float q;
+
+		r = i/k;
+		j = i;
+		m = k;
+		q = (j/m) - r;
+
+		if (q > 0.0)
+		{
+			return 1;
+		}
+    return 0;
+	}
+}
+
+int main()
+{
+    printf(" введите число N\n" );
+    scanf(" %d", &n);
+    while (n < 2)
+    {
+       printf(" N не простое ");
+    }
+   for(i = 2; i <= n; i++)
+   {
+        if (prostota( i ) == 1 )
+        {
+            printf (" %d\n", i );
+        }
+
+   }
+}
