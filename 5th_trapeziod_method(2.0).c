4a160362cb8734fadfcd8bb47fea890edddb5590
Vio-lettaB <126187797+Vio-lettaB@users.noreply.github.com>
2023-03-22 00:37:15 +0700

    Update 5th_trapeziod_method(2.0).c

--- 5th_trapeziod_method(2.0).c
+++ 5th_trapeziod_method(2.0).c
@@ -4,29 +4,29 @@
 
 float Func(float x)
 {
-   float res;
+   float res = 0;
    res = sin(x);  
    return res;
 }
 
 float integral( float a, float b, int n)
 {
-    float step;
+    float step = 0 ;
     step = (b - a) / n;
     printf(" длина разбиения: %f\n", step );
-    float summid;
+    float summid = 0 ;
    
     if ( step > 0 )
     {
          for ( float i = a; i < b ; i += step)
         {
             
-            float mid;
+            float mid = 0;
             
             mid = ( Func(i) + Func( i + step )) * step / 2;
-            // printf(" Промежуточное значение средней линии трапеци: %f\n", mid);
+             printf(" Промежуточное значение средней линии трапеци: %f\n", mid);
             summid += mid;
-            // printf(" Промежуточное значение средней линии трапеци: %f\n", summid);
+             printf(" Промежуточное значение средней линии трапеци: %f\n", summid);
             
         }
     return summid;
@@ -36,7 +36,7 @@ float integral( float a, float b, int n)
         for ( float i = b; i < a ; i -= step)
         {
             
-            float mid;
+            float mid = 0;
             
             mid = ( Func(i) + Func( i + step )) * step / 2;
             // printf(" Промежуточное значение средней линии трапеци: %f\n", mid);
@@ -48,13 +48,12 @@ float integral( float a, float b, int n)
 
 int main()
 {
-    int n_step;
-    float a = 4;
-    float b = 88;
+    int n_step = 0;
+    float a = 0;
+    float b = 6.28 ;
     printf(" Введите шаг разбиения:" );
     scanf("%d", &n_step);
     // printf("Введите по очереди пределы интегрирования:");
-    // scanf("%f %f", &a, &b);
+    //  scanf("%f %f", &a, &b);
     printf(" значение интеграла от а = %f  до  b = %f равно = %f", a, b, integral(a, b, n_step));
 }
-
