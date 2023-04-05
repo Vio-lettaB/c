d1ab6c461496ae60cf3e983c86d3199a7218defe
Vio-lettaB <126187797+Vio-lettaB@users.noreply.github.com>
2023-04-05 10:47:33 +0700

    Update 6th_static_array(draft_version).c

--- 6th_static_array(draft_version).c
+++ 6th_static_array(draft_version).c
@@ -27,13 +27,40 @@ int array_output( int size )
   }
 }
 
+int Max( int size)
+{
+    int max = 0;
+    for(int i = 0; i < size; ++i)
+        {
+            if(a[i] > max)
+            {
+                max = a[i];
+            }
+        }
+    return max;
+}
+
+
+int Min( int size )
+{
+    int min = a[0];
+    for(int i = 0; i < size; ++i)
+        {
+            if(a[i] < min)
+            {
+                min = a[i];
+            }
+        }
+    return min;
+}
 int main()
 {
   setting_an_array();
   array_output(size); 
   
-  int max = 0;
-  int min = 0;
+ 
   int average =0;
   int rms = 0;
+  printf(" max = %d", Max(size));
+  printf(" min = %d", Min(size));
 } 
