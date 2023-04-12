547c1908104c21d4dba1c34bf3f0f8c6e0b783c2
Vio-lettaB <126187797+Vio-lettaB@users.noreply.github.com>
2023-04-12 22:01:38 +0700

    Update 8th_sort_array(2_in_1).c

--- 8th_sort_array(2_in_1).c
+++ 8th_sort_array(2_in_1).c
@@ -126,7 +126,7 @@ int swap_if3(int *arr, int len, int i)
     }
 }
 
-int Bigsort(int (*sort)(int *, int ), int (*swapif)(int *, int, int), int *arr, int len)
+int Bigsort(int(*sort)(int *, int ), int(*swapif)(int *, int, int), int *arr, int len)
 {
     int i = 0;
     while (sort(arr, len) == 0)
@@ -140,7 +140,7 @@ int Bigsort(int (*sort)(int *, int ), int (*swapif)(int *, int, int), int *arr, 
 }
 
 
-int print_array (int *arr, int len)
+int print_array(int *arr, int len)
 {
     for (int i = 0; i < len; i++)
     {
@@ -166,13 +166,13 @@ int main()
     }
     
     print_array(arr, len);
-    Bigsort(sort1(arr, len), swap_if1(arr, len, 0), arr, len);
+    Bigsort(sort1, swap_if1, arr, len);
     printf("\nотсортированный массив 1: ");
     print_array(arr, len);
-    Bigsort(sort2(arr, len), swap_if2(arr, len, 0), arr, len);
+    Bigsort(sort2, swap_if2, arr, len);
     printf("\nотсортированный массив 2: ");
     print_array(arr, len);
-    Bigsort(sort3(arr, len), swap_if3(arr, len, 0), arr, len);
+    Bigsort(sort3, swap_if3, arr, len);
     printf("\nотсортированный массив 3: ");
     
     
@@ -188,4 +188,3 @@ int main()
     // print_array(arr, len);
     
 }
-
