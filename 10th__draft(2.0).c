57773a16f8db1898505731dbba4478e58e14a1b0
Vio-lettaB <126187797+Vio-lettaB@users.noreply.github.com>
2023-05-10 17:58:32 +0700

    Update 10th__draft(2.0).c

--- 10th__draft(2.0).c
+++ 10th__draft(2.0).c
@@ -126,7 +126,7 @@ Matrix summ(Matrix *matA, Matrix *matB)
 {   
     if( matA->row_size != matB->row_size || matA->col_size != matB->col_size )
     {
-       printf("матрицы имеют различные размеры, суммирование не возможно!(");
+       printf("матрицы имеют различные размеры, суммирование не возможно!(\n");
     }
     else
     {
@@ -155,7 +155,7 @@ Matrix summ(Matrix *matA, Matrix *matB)
             }
             col_idx++;
         }
-        
+        printf("сумма матриц A и B:\n\n");
         return matC;  
     }
 }
@@ -164,14 +164,14 @@ Matrix summ(Matrix *matA, Matrix *matB)
 
 Matrix mult(Matrix *matA, Matrix *matB)
 {
+    
     if( matA->row_size != matB->col_size )
     {
-       printf("матрицы имеют различные размеры, умножение не возможно!(");
+       printf("матрицы имеют различные размеры, умножение не возможно!(\n");
     }
     else
     {
-        Matrix matC = init_matrix(matA->col_size, matB->row_size) ;
-        PrintMatrix(&matC);
+        Matrix matC = init_matrix(matA->col_size, matB->row_size);
         int col_idx = 0;
         while ( col_idx < matC.col_size )
         {
@@ -182,7 +182,7 @@ Matrix mult(Matrix *matA, Matrix *matB)
                 int k = 0;
                 while ( k < matC.col_size )
                 {
-                    matC.A[matA->row_size * col_idx + row_idx] +=  matA->A[matA->row_size * col_idx + k] +  matB->A[matB->row_size * k + row_idx];
+                    matC.A[matA->row_size * col_idx + row_idx] += ( matA->A[matA->row_size * col_idx + k] *  matB->A[matB->row_size * k + row_idx]);
                     // printf(" IJ of A %lf\n", matA->A[matA->row_size * k + row_idx]);
                     // printf(" IJ of B %lf\n", matB->A[matA->row_size * col_idx + k]);
                     k++;
@@ -191,6 +191,7 @@ Matrix mult(Matrix *matA, Matrix *matB)
             }
             col_idx++;
         }
+        printf("произведение матриц A и B:\n\n");
         return matC;  
     }
 }
@@ -203,56 +204,58 @@ Matrix make_minor(Matrix *matA, int i, int j)
     {
        for(int row_idx = 0; row_idx < matB.row_size;  row_idx++ )
        {
-          if(row_idx < i && col_idx < j) 
-          {
-            matB.A[matA->row_size * col_idx + row_idx] = matA->A[matA->row_size * col_idx + row_idx];
-          }
-          if (row_idx >= i )
-          {
-            matB.A[matA->row_size * col_idx + row_idx] = matA->A[matA->row_size * (col_idx + 1) + row_idx];
-          }
-          if (col_idx >= j )
-          {
-            matB.A[matA->row_size * col_idx + row_idx] = matA->A[matA->row_size * col_idx + (row_idx + 1)];
-          }
-          if(row_idx >= i && col_idx >= j) 
-          {
-            matB.A[matA->row_size * col_idx + row_idx] = matA->A[matA->row_size * (col_idx + 1) + (row_idx + 1)];
-          }
-          
-          printf( "%lf\t",  matB.A[matA->row_size * col_idx + row_idx] );
-          
+            if(row_idx < i && col_idx < j) 
+            {
+                matB.A[matA->row_size * col_idx + row_idx] = matA->A[matA->row_size * col_idx + row_idx];
+            }
+            if (row_idx >= i )
+            {
+                matB.A[matA->row_size * col_idx + row_idx] = matA->A[matA->row_size * (col_idx + 1) + row_idx];
+            }
+              if (col_idx >= j )
+            {
+                matB.A[matA->row_size * col_idx + row_idx] = matA->A[matA->row_size * col_idx + (row_idx + 1)];
+            }
+            if(row_idx >= i && col_idx >= j) 
+            {
+                matB.A[matA->row_size * col_idx + row_idx] = matA->A[matA->row_size * (col_idx + 1) + (row_idx + 1)];
+            }
+        // printf( "%lf\t",  matB.A[matA->row_size * col_idx + row_idx] );
        }
-          printf("\n");
+        // printf("\n");
     }
-      printf("\n");
+    // printf("\n");
     return matB;
 }
 
 
 
-int det(Matrix *matA)
+double det(Matrix *matA)
 {   
    
     if( matA->row_size != matA->col_size)
     {
-        printf("матрица вырожденная!(\n"); 
+        printf("матрица не квадратная!(\n"); 
         return 0;
     }
     else
     {
-        if(  matA->row_size == 2)
+        
+        if(  matA->col_size == 2)
         {
             int d = 0;
             int x = 0;
             int y = 0;
-            if (matA->A[matA->row_size * 0 + 0]  != 0 && matA->A[matA->row_size * matA->col_size + matA->row_size] != 0 )
+            // printf("help!!!\n");
+            if (matA->A[0]  != 0 && matA->A[3] != 0 )
             {
-                x = matA->A[matA->row_size * 0 + 0] * matA->A[matA->row_size * matA->col_size + matA->row_size]; 
+                x = matA->A[0] * matA->A[3]; 
+                // printf(" x = %d\n", x);
             }
-            if (matA->A[matA->row_size * 0 + matA->row_size]  != 0 && matA->A[matA->row_size * matA->col_size + 0] != 0 )
+            if (matA->A[2]  != 0 && matA->A[1] != 0 )
             {
-                y = matA->A[matA->row_size * 0 + matA->row_size] * matA->A[matA->row_size * matA->col_size + 0]; 
+                y = matA->A[2] * matA->A[1]; 
+                // printf(" y = %d\n", y);
             }
             return d = x - y;
         }
@@ -261,12 +264,12 @@ int det(Matrix *matA)
             int d = 0;
             for ( int col_idx = 0; col_idx < matA->col_size; col_idx++ )
             {
-                printf("%d\n", col_idx);
+                // printf("%d\n", col_idx);
                 for ( int row_idx = 0; row_idx < matA->row_size; row_idx++ )
                 {
                     Matrix matB = make_minor( matA, row_idx, col_idx);
-                    printf("\n");
-                    d += pow(- 1, col_idx ) * matA->A[matA->row_size * col_idx + row_idx] * det(&matB);
+                    // printf("\n");
+                    d += pow(- 1, row_idx ) * matA->A[matA->row_size * col_idx + row_idx] * det(&matB);
                 }
             }
             return d;
@@ -276,11 +279,11 @@ int det(Matrix *matA)
 
 
 
-Matrix inverse(Matrix *matA, int d)
+Matrix inverse(Matrix *matA, double d)
 {
     if( matA->row_size != matA->col_size)
     {
-        printf("матрицa не квадратная, нахождение обратной матрицы не возможно!!(\n"); 
+        printf("матрицa не квадратная, нахождение обратной матрицы не возможно!(\n"); 
     }
     else
     {
@@ -291,14 +294,15 @@ Matrix inverse(Matrix *matA, int d)
             for( int row_idx = 0; row_idx < matA->row_size; row_idx++)
             {
                 Matrix matC = make_minor(matA, col_idx, row_idx);
-                // printf("minor\n");
+                PrintMatrix(&matC);
                 // print_minor(&m);
                 // printf("\n");
-                matB.A[matA->row_size * col_idx + row_idx] = pow(- 1, col_idx + row_idx) * (double) det(&matC)/(double) d;
+                matB.A[matB.col_size * row_idx + col_idx] = pow(- 1, col_idx + row_idx) * (double) det(&matC)/(double) d;
                 // printf("det minor = %d\n\n", det_minor(&m));
             }
         }
         // printf("\n");
+        printf("обратная к матрице A:\n\n");
         return matB;
     }
 }
@@ -378,7 +382,7 @@ int main()
     Matrix A = make_diagonal_matrix( col_size1, row_size1);
     Matrix B = make_diagonal_matrix( col_size2, row_size2);
     Matrix C;
-    int DET;
+    double DET;
     
     PrintMatrix(&A);
     PrintMatrix(&B);
@@ -389,10 +393,11 @@ int main()
     
     C = mult(&A, &B);
     PrintMatrix(&C);
-    destroy_matrix(&C);
+    // destroy_matrix(&C);
     
     DET = det(&A);
-    printf("det a = %d\n", DET);
+    printf("det a = %lf\n\n", DET);
+    // destroy_matrix(&C);
     C = inverse(&A, DET);
     PrintMatrix(&C);
     destroy_matrix(&C);
